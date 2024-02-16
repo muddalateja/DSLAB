051f340ae91f120e8b18913bb757e443077ea680
muddalateja <160223683+muddalateja@users.noreply.github.com>
2024-02-16 14:57:42 +0530

    Update BinSearch.c

--- BinSearch.c
+++ BinSearch.c
@@ -8,25 +8,25 @@ int main()
 	for(i=0;i<n;i++)
 		scanf("%d",&a[i]);
 	printf("enter searching element:");
-scanf("%d",&se);
-for(i=0;i<n;i++)
-lb=0;
-ub=n-1;
-while(lb<=ub)
-{
-mid=(lb+ub)/2;
-if(se==a[mid])
-{
-printf("searching element is found at location %d",mid+1);
-t=1;
-break;
-}
-else
-if(se>a[mid])
-lb=mid+1;
-else
-ub=mid-1;
-}
-if(t==0)
-printf("searching element is not found");
+    scanf("%d",&se);
+    for(i=0;i<n;i++)
+    lb=0;
+    ub=n-1;
+    while(lb<=ub)
+        {
+        mid=(lb+ub)/2;
+    if(se==a[mid])
+        {
+        printf("searching element is found at location %d",mid+1);
+        t=1;
+        break;
+        }
+        else
+    if(se>a[mid])
+        lb=mid+1;
+    else
+        ub=mid-1;
+        }
+    if(t==0)
+        printf("searching element is not found");
 }
