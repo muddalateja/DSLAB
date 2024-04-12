e8e49f768063c8327c5bbf3ca50bbe95defd79d0
muddalateja <160223683+muddalateja@users.noreply.github.com>
2024-04-12 15:22:12 +0530

    Stack using array.c

--- Stack using array.c
+++ Stack using array.c
@@ -3,39 +3,41 @@ void main(){
 int a[100],ch,n,i,top=-1;
 do
 {
-    printf("\nStack operations:");
-    printf("\n1.push\n2.pop\n3.display\n4.exit");
-    printf("\nEnter your choice:");
-    scanf("%d",&ch);
+printf("\nStack operations:");
+printf("\n1.push\n2.pop\n3.display\n4.exit");
+printf("\nEnter your choice:");
+scanf("%d",&ch);
 switch(ch)
 {
 case 1: if(top==a[i])
-            printf("Stack is full");
-            else
-            {
-            printf("Enter element to push:");
-            scanf("%d",&n);
-            top++;
-            a[top]=n;
-             }
+        printf("Stack is full");
+        else
+        {
+        printf("Enter element to push:");
+        scanf("%d",&n);
+        top++;
+        a[top]=n;
+        }
         break;
 case 2: if(top==-1)
-            printf("Stack is empty");
-            else
-            {
-            n=a[top];
-            printf("Delected element is %d",n);
-            top--;
-            }
+        printf("Stack is empty");
+        else
+        {
+        n=a[top];
+        printf("Delected element is %d",n);
+        top--;
+        }
         break;
 case 3: if(top==-1)
-            printf("Stack is empty");
-            else
-            for(i=top;i>=0;i--)
-            printf("\n%d",a[i]);
-            }
-            }while(ch!=4);
-            }
-         
+        printf("Stack is empty");
+        else
+        for(i=top;i>=0;i--)
+        printf("\n%d",a[i]);
+        break;
+        default: printf("\nInvalid choice");
+        }
+        }while(ch!=4);
+        }
+        
         
         
